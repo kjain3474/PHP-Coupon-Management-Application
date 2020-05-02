0f85c44a5beb6d828ddce8ff743dc3ee6c836ee3
Kapil Jain <kjain3474@gmail.com>
2020-05-02 13:00:54 +0530

    config file changed to heroku DB

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,8 @@
 <?php
-  define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
-  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+  define( 'DB_HOST', 'us-cdbr-east-06.cleardb.net' );          // Set database host
+  define( 'DB_USER', 'bb2f10ac95fc2a' );             // Set database user
+  define( 'DB_PASS', '656ce21c' );             // Set database password
+  define( 'DB_NAME', 'heroku_5c9616b29a1aedb' );        // Set database name
+
 
 ?>
